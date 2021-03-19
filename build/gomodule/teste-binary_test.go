0994dfd97131f71016344eb53b003b713a8cbad0
SlavaIvanchenko17 <torstein.viking.2020@gmail.com>
2021-03-19 11:36:05 +0200

    testing teste-binary

--- build/gomodule/teste-binary_test.go
+++ build/gomodule/teste-binary_test.go
@@ -1,10 +1,14 @@
 package gomodule
 
 import (
+	"bytes"
 	"fmt"
 	"reflect"
+	"strings"
 	"testing"
 
+	"github.com/google/blueprint"
+	"github.com/roman-mazur/bood"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -46,4 +50,56 @@ func TestResolve(t *testing.T) {
 	resolved, unresolved := resolvePatterns(&plug, patters, exclude)
 	assert.True(t, reflect.DeepEqual(resolved, []string{"c"}))
 	assert.True(t, reflect.DeepEqual(unresolved, []string{"b"}))
+}
+
+func TestBinFactory(t *testing.T) {
+	ctx := blueprint.NewContext()
+
+	ctx.MockFileSystem(map[string][]byte{
+		"Blueprints": []byte(`
+			go_binary {
+			  name: "test-out",
+			  srcs: ["test-src.go"],
+			  pkg: ".",
+			  testPkg: ".",
+	          vendorFirst: true,
+			  srcsExclude: ["**/*_test.go"],
+              testSrcs: ["**/*_test.go"],
+			}
+		`),
+		"test-src.go": nil,
+	})
+
+	ctx.RegisterModuleType("go_binary", BinFactory)
+
+	cfg := bood.NewConfig()
+
+	_, errs := ctx.ParseBlueprintsFiles(".", cfg)
+	if len(errs) != 0 {
+		t.Fatalf("Syntax errors in the test blueprint file: %s", errs)
+	}
+
+	_, errs = ctx.PrepareBuildActions(cfg)
+	if len(errs) != 0 {
+		t.Errorf("Unexpected errors while preparing build actions: %s", errs)
+	}
+	buffer := new(bytes.Buffer)
+	if err := ctx.WriteBuildFile(buffer); err != nil {
+		t.Errorf("Error writing ninja file: %s", err)
+	} else {
+		text := buffer.String()
+		t.Logf("Gennerated ninja build file:\n%s", text)
+		if !strings.Contains(text, "out/bin/test-out: ") {
+			t.Errorf("Generated ninja file does not have build of the test module")
+		}
+		if !strings.Contains(text, "test-src.go") {
+			t.Errorf("Generated ninja file does not have source dependency")
+		}
+		if !strings.Contains(text, "build vendor: g.gomodule.vendor | go.mod") {
+			t.Errorf("Generated ninja file does not have vendor build rule")
+		}
+		if !strings.Contains(text, "build out/.test-out.test.out") {
+			t.Errorf("Generated ninja file does not have gotest rule")
+		}
+	}
 }
